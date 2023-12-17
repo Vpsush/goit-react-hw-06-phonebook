c474fd8cbe74330a95bbb35a395ab3adb82850cf
Vpsush <vpsush2023@gmail.com>
2023-12-17 11:17:03 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import React, { useState } from 'react';
 import ContactForm from './components/ContactForm/ContactForm';
 import Filter from 'components/Filter/Filter';
 import { Contact } from './components/Contact/Contact';
 
 export default function App() {
-  const [filter, setFilter] = useState('');
-
   return (
     <div>
       <section>
         <ContactForm />
       </section>
       <section>
-        <Filter value={filter} onChange={e => setFilter(e.target.value)} />
+        <Filter />
       </section>
       <Contact />
     </div>
